90a9bbfb5f02bc3a232a5e2e9a0d0df86bf42114
Jacob-CL <jclummus.w@gmail.com>
2024-05-12 08:16:45 +1000

    Working update
    AStar_GC successfully logs entire file when found and then proceeds to listen for new log lines

--- LogSender.py
+++ LogSender.py
@@ -24,22 +24,29 @@ files = [
 ############################################################################################################################
 
 def monitor_log_file(log_file_path):
-    try:        
-        with open(log_file_path, "r") as logfile:
-            for line in logfile:
-                
-            for line in tailer.follow(file):
-            #     # Process the latest line from the CSV file
-            #     reader = csv.reader([logfile])
-            #     for row in reader:
+    while True:
+        try:        
+            with open(log_file_path, "r") as logfile:
+                if log_file_path == "C:\\Users\\User\\AppData\\Local\\Firaxis Games\\Sid Meier\'s Civilization VI\\Logs\\AStar_GC.log":
+                    print('Found log file..')
+                    AStarGC_json = convert_logfile_data_to_json(log_file_path)
+                    send_it("Custom-AStarGC_CL", AStarGC_json)
+                    print("Sent log file!")
+                    print("Now listening for new lines...")
 
-                if logfile == "C:\\Users\\User\\AppData\\Local\\Firaxis Games\\Sid Meier\'s Civilization VI\\Logs\\AStar_GC.log":
-                    print('Yep, you got me')
-                    AStarGC_json_output = convert_AStarGC_to_json(log_file_path, 'output.json')
-                    send_it("Custom-AStarGC_CL", AStarGC_json_output)
+                    # Now continue to monitor for new lines
+                    for line in tailer.follow(logfile):
+                        print("I found a new line!")
+                        line_json = convert_new_logline_to_json(log_file_path, line)
+                        send_it("Custom-AStarGC_CL", line_json)
+                        print("New lines sent!")                 
 
-    except HttpResponseError as e:
-        print(f"Upload failed: {e}")
+        except HttpResponseError or FileNotFoundError as e:
+                if FileNotFoundError:
+                    print(f"File {log_file_path} not found, checking again in 1 seconds.")
+                    time.sleep(1)  # Wait for some time before checking again, e.g., 10 seconds
+                else:
+                    print(f"Failed: {e}")
 
 ############################################################################################################################
 ############################################################################################################################
@@ -81,7 +88,7 @@ def start_monitoring(files):
 ############################################################################################################################
 ############################################################################################################################
 
-def convert_AStarGC_to_json(log_file_path):
+def convert_logfile_data_to_json(log_file_path):
     # Open the log file
     with open(log_file_path, 'r') as log_file:
         # Read all lines from the file
@@ -91,23 +98,52 @@ def convert_AStarGC_to_json(log_file_path):
     log_data = []
     for line in lines[1:]:
         parts = line.strip().split(',')
-        log_entry = {
-            "GameTurn": parts[0].strip(),
-            "Player": parts[1].strip(),
-            "Unit": parts[2].strip(),
-            "FromX": int(parts[3].strip()),
-            "FromY": int(parts[4].strip()),
-            "ToX": int(parts[5].strip()),
-            "ToY": int(parts[6].strip()),
-            "Info": int(parts[7].strip()),
-            "Checksum": parts[8].strip()
-        }
+        if log_file_path == "C:\\Users\\User\\AppData\\Local\\Firaxis Games\\Sid Meier\'s Civilization VI\\Logs\\AStar_GC.log":
+            log_entry = {
+                "GameTurn": parts[0].strip(),
+                "Player": parts[1].strip(),
+                "Unit": parts[2].strip(),
+                "FromX": int(parts[3].strip()),
+                "FromY": int(parts[4].strip()),
+                "ToX": int(parts[5].strip()),
+                "ToY": int(parts[6].strip()),
+                "Info": int(parts[7].strip()),
+                "Checksum": parts[8].strip()
+            }
+
         log_data.append(log_entry)
 
     # Convert the list of dictionaries to JSON format
     json_data = json.dumps(log_data)
 
     return json_data
+
+############################################################################################################################
+############################################################################################################################
+
+def convert_new_logline_to_json(log_file_path, line):
+    with open(log_file_path, "r") as logfile: 
+        log_data = []
+        parts = line.strip().split(',')
+        if log_file_path == "C:\\Users\\User\\AppData\\Local\\Firaxis Games\\Sid Meier\'s Civilization VI\\Logs\\AStar_GC.log":
+            log_entry = {
+                "GameTurn": parts[0].strip(),
+                "Player": parts[1].strip(),
+                "Unit": parts[2].strip(),
+                "FromX": int(parts[3].strip()),
+                "FromY": int(parts[4].strip()),
+                "ToX": int(parts[5].strip()),
+                "ToY": int(parts[6].strip()),
+                "Info": int(parts[7].strip()),
+                "Checksum": parts[8].strip()
+            }
+        log_data.append(log_entry)
+    
+    # Convert the list of dictionaries to JSON format
+    json_data = json.dumps(log_data)
+
+    return json_data
+
 
 ############################################################################################################################
 ############################################################################################################################
